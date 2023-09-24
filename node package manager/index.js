506277663ad43f7ed1c55fdad6ccf5b536353e48
Amien kurniawan <offdevamienk@gmail.com>
2023-09-24 11:22:58 +0700

    update => create, publish, and use new library

--- node package manager/index.js
+++ node package manager/index.js
@@ -1,5 +1,12 @@
+import { findBigest, sayHello, sum } from "amien-node-npm-library";
+
 import { readFile } from "node-package-manager/read";
 import { writeTofile } from "node-package-manager/write";
 
 writeTofile("amien.log", "amien kurniawan");
 readFile("amien.log");
+
+console.info(sayHello("TEST INI AMIEN KURNIAWAN"));
+
+console.info(sum([12, 12, 1]));
+console.info(findBigest([12, 15, 1]));
