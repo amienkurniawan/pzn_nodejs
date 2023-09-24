fa52c3d1de1ddf618ccc1aa60911da6432912c1e
Amien kurniawan <offdevamienk@gmail.com>
2023-09-24 19:27:36 +0700

    update => add jest configuration

--- unit-test/test/sum.test.js
+++ unit-test/test/sum.test.js
@@ -5,3 +5,13 @@ test("Sum(1,2) must be 3", () => {
 
   expect(result).toBe(3);
 });
+test("Sum(1,2) must be 3 1", () => {
+  const result = sum(1, 2);
+
+  expect(result).toBe(3);
+});
+test("Sum(1,2) must be 3 2", () => {
+  const result = sum(1, 2);
+
+  expect(result).toBe(3);
+});
