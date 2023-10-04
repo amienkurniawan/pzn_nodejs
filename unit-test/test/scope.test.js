cb9cc2299c25c6ba103b9eb4786db6b119f163f2
Amien kurniawan <offdevamienk@gmail.com>
2023-10-04 20:46:53 +0700

    update => test scope

--- unit-test/test/scope.test.js
+++ unit-test/test/scope.test.js
@@ -1,17 +1,21 @@
 beforeAll(() => console.info("Before All Outer"));
-afterAll(() => console.info("After All Outer"));
 beforeEach(() => console.info("Before Each Outer"));
-afterEach(() => console.info("After Each Outer"));
 
 test("Test Outer", () => console.info("Test Outer"));
+afterEach(() => console.info("After Each Outer"));
 
 describe("inner", () => {
 
   beforeAll(() => console.info("Before All inner"));
-  afterAll(() => console.info("After All inner"));
   beforeEach(() => console.info("Before Each inner"));
-  afterEach(() => console.info("After Each inner"));
+
+  afterAll(() => console.info("After All inner"));
 
   test("Test Inner", () => console.info("Test Inner"));
 
+  afterEach(() => console.info("After Each inner"));
+
 });
+
+
+afterAll(() => console.info("After All Outer"));
