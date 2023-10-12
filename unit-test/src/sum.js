2d0e86bf88ad59fd1861ec18795890cd89083539
Amien kurniawan <offdevamienk@gmail.com>
2023-10-12 22:34:15 +0700

    mock function

--- unit-test/src/sum.js
+++ unit-test/src/sum.js
@@ -11,3 +11,19 @@ export const sumAll = (myNumbers) => {
 
   return sums;
 }
+
+export const calculate = (numbers, callback) => {
+  let total = 0;
+  for (let number of numbers) {
+    total += number;
+  }
+  callback(total);
+}
+
+export const calculateAndReturn = (numbers, callback) => {
+  let total = 0;
+  for (let number of numbers) {
+    total += number;
+  }
+  return callback(total);
+};
