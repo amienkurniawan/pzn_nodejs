d200da831725a3ceb510ea7e82aca7f292e2b3cf
Amien kurniawan <offdevamienk@gmail.com>
2023-10-23 22:26:21 +0700

    update => add mock class

---
+++ unit-test/src/user-repository.js
@@ -0,0 +1,13 @@
+export class UserRepository {
+  save(user) {
+    throw new Error("Not Implemented")
+  }
+
+  findById(id) {
+    throw new Error("not implemented")
+  }
+
+  findAll() {
+    throw new Error("not implemented")
+  }
+}
