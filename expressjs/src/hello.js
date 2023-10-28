83532ef4d9bd1be778d0be453655ae763b178c63
Amien kurniawan <offdevamienk@gmail.com>
2023-10-28 20:32:55 +0700

    update => basic routing

--- expressjs/src/hello.js
+++ expressjs/src/hello.js
@@ -2,6 +2,15 @@ import express from "express";
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send("hello world");
+})
+
+app.get('/amien', (req, res) => {
+  console.log(req)
+  res.send("hello world amien");
+})
+
 app.listen(3000, () => {
   console.log("server running in port 3000")
 });
