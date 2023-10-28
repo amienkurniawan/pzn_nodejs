e3040960b75c44caae3723911f50aa6bfa1c8c57
Amien kurniawan <offdevamienk@gmail.com>
2023-10-28 16:41:41 +0700

    update => rejections

---
+++ logging/src/logging-rejection.js
@@ -0,0 +1,19 @@
+import winston from "winston";
+
+const logger = winston.createLogger({
+  level: 'info',
+  transports: [
+    new winston.transports.Console({}),
+    new winston.transports.File({
+      handleExceptions: true,
+      handleRejections: true,
+      filename: "rejection.log"
+    })
+  ]
+});
+
+async function callAsync() {
+  return Promise.reject("ups");
+}
+
+callAsync();
