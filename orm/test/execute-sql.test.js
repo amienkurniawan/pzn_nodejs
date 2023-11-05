e81f10f66182d4b186f06c57e256199fd82281c2
Amien kurniawan <offdevamienk@gmail.com>
2023-11-05 20:52:33 +0700

    update => query SQL

--- orm/test/execute-sql.test.js
+++ orm/test/execute-sql.test.js
@@ -2,7 +2,7 @@ import { prismaClient } from "../src/prisma-client";
 
 describe("Prisma Client", () => {
   it("should be able to execute sql", async () => {
-    const name = "Amien Kurniawan";
+    const name = "Amien Kurniawan 2";
 
     const impacted = await prismaClient.$executeRaw`INSERT INTO sample(name) VALUES (${name});`;
     expect(impacted).toBe(1);
