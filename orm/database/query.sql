27e26a3c84ff49c51a2d48450a208ee14a294e05
Amien kurniawan <offdevamienk@gmail.com>
2023-11-10 07:22:02 +0700

    update => adding products model

--- orm/database/query.sql
+++ orm/database/query.sql
@@ -19,6 +19,24 @@ create table customers(
 
 SELECT * FROM sample;
 
+create table products(
+  id varchar(100) not null,
+  name varchar(100) not null,
+  price int not null,
+  stock int not null,
+  category varchar(100) not null,
+  primary key (id)
+)
+
+insert into products (id,name, price, stock, category) 
+values 
+('P0001','A',1000,100,'K1'),
+('P0002','B',2000,200,'K1'),
+('P0003','C',3000,300,'K1'),
+('P0004','D',4000,400,'K2'),
+('P0005','E',5000,500,'K2');
+
+
 SELECT * FROM customers;
 use belajar_nodejs_database;
 DESC TABLE customers;
